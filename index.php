e98caf14bfc89c152074eb4bc6b90f7153b5c808
Madushan Sandaruwan <madushansandaru1@gmail.com>
2021-05-21 22:59:34 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+    function sendMsg($to, $msg) {
+        $user = "94771637551";
+        $password = "3803";
+
+        $text = urlencode($msg);
+
+        $baseurl ="http://www.textit.biz/sendmsg";
+        $url = "$baseurl/?id=$user&pw=$password&to=$to&text=$text";
+        $ret = file($url);
+
+        $res = explode(":",$ret[0]);
+
+        if (trim($res[0])=="OK")
+        {
+            return true;
+//            echo "Message Sent - ID : ".$res[1];
+        }
+        else
+        {
+            return false;
+//            echo "Sent Failed - Error : ".$res[1];
+        }
+    }
+
+    $status = false;
+
+    if(isset($_POST['send'])) {
+        $phone = $_POST['phone'];
+        $msg = $_POST['msg'];
+
+        $status = sendMsg($phone, $msg);
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Textit.biz</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">
+    <h2 class="mt-5">SMS Gateway Test</h2>
+
+    <div class="my-3">
+
+        <?php
+
+        if($status && isset($_POST['send'])) {
+        ?>
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                </svg>
+                <div>
+                    Successful.
+                </div>
+            </div>
+        <?php
+        } elseif(isset($_POST['send'])) {
+        ?>
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg>
+                <div>
+                    Failed.
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+
+    </div>
+
+    <form action="index.php" method="post">
+        <div class="mb-3">
+            <label for="phone" class="form-label">Phone Number</label>
+            <input type="text" class="form-control" id="phone" name="phone" maxlength="11" minlength="11" placeholder="94XXXXXXXXX" required>
+        </div>
+        <div class="mb-3">
+            <label for="msg" class="form-label">Message</label>
+            <textarea class="form-control" id="msg" rows="3" name="msg" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary" name="send">Send</button>
+    </form>
+
+</div>
+
+
+</body>
+</html>
+
